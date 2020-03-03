c4ba26aaa65e2ea87313f6094a692acebd8dd305
DerianEduardo <derian_10dyt@hotmail.com>
2020-03-03 00:09:25 -0600

    maquetacion login,registro,notpage,navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { RegisterComponent } from './componentes/register/register.component';
 import { LoginComponent } from './componentes/login/login.component';
+import { PrivadoComponent } from './componentes/privado/privado.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'privado', component: PrivadoComponent},
+  {path: '**', component: NotFoundPageComponent}
 ];
 
 @NgModule({
